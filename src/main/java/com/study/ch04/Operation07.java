6e4685091e1262d1adb4af534579d4ed83c20900
kangjy311 <kangjy311@naver.com>
2024-01-02 17:10:38 +0900

    수업 실습

--- src/main/java/com/study/ch04/Operation07.java
+++ src/main/java/com/study/ch04/Operation07.java
@@ -1,2 +1,20 @@
-package com.study.ch04;public class Operation07 {
+package com.study.ch04;
+
+public class Operation07 {
+    /*
+        1. Operation07.java 파일 생성
+        2. 깃, 깃허브 수업 실습 println
+        3. add, commit, push
+        4. 자리 바꾸기
+        5. 계정 변경
+        6. 클론 (폴더명 java_study_본인이름영어로)
+        7. Operation07.java 파일에서 "변경사항 추가" println
+        8. add, commit, push
+        9. 다시 자기 자리로 돌아오기
+        10. 계정 변경
+        11. fetch, pull
+     */
+    public static void main(String[] args) {
+        System.out.println("깃, 깃허브 수업 실습");
+    }
 }
